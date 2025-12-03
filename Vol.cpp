4bbaa1bc52da3cacfc363e14d7b2f52418a148cf
adriajofre19 <118483942+adriajofre19@users.noreply.github.com>
2025-12-03 14:17:19 +0000

    first

---
+++ Vol.cpp
@@ -0,0 +1,24 @@
+#include "Vol.h"
+
+Vol::Vol(int id, int hora, int minut, char tipus) 
+    :id(id), hora(hora), minut(minut), tipus(tipus)
+{
+
+}
+
+int Vol::get_id() const {
+    return id;
+}
+
+int Vol::get_hora() const {
+    return hora;
+}
+
+int Vol::get_minut() const {
+    return minut;
+}
+
+char Vol::get_tipus() const {
+    return tipus;
+}
+
---
+++ Vol.h
@@ -0,0 +1,24 @@
+#ifndef VOL_H
+#define VOL_H
+
+#include <string>
+using namespace std;
+
+class Vol {
+private:
+    int id, hora, minut;
+    char tipus;
+
+public:
+    // Constructor
+    Vol(int id, int hora, int minut, char tipus);
+
+    // Getters
+    int get_id() const;
+    int get_hora() const;
+    int get_minut() const;
+    char get_tipus() const;
+
+};
+
+#endif
