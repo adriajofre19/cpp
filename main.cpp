d0d33d11912fc11bd758f9a5b04514874865f903
adriajofre19 <118483942+adriajofre19@users.noreply.github.com>
2025-12-03 14:49:23 +0000

    test candidats

--- main.cpp
+++ main.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
 #include "Vol.h"
 #include "Candidats.h"
+#include <list>
 
 using namespace std;
 
 int main() {
-    
     for(int i = 0; i < 10; i++) {
         Vol v(i,'i',10,30);
         v.imprimir_vol();
     }
-    
+
+    // Prova de la classe Candidats amb enters
+    list<int> nums = {10, 20, 30, 40};
+    Candidats<int> c(nums);
+    while (!c.fi()) {
+        std::cout << "  actual = " << c.actual() << '\n';
+        c.seguent();
+    }
+
     return 0;
 }
